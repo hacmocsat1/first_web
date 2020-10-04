f4694f4dffcdf6098e4024f84c60a94d305f6ae4
hacmocsat5@gmail.com <hacmocsat5@gmail.com>
2020-10-04 16:35:14 +0700

    initial commit

---
+++ shop/resources/views/admin/All.blade.php
@@ -0,0 +1,66 @@
+@extends('admin_layout')
+@section('admin_content')
+
+<div class="table-agile-info">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      LIỆT KÊ DANH MỤC SẢN PHẨM
+    </div>
+    <div class="table-responsive">
+      <table class="table table-striped b-t b-light">
+        <thead>
+          <tr>
+            <th style="width:20px;">
+              <label class="i-checks m-b-none">
+              </label>
+            </th>
+            <th>Tên danh mục</th>
+            <th>Hiển thị</th>
+            <th style="width:30px;"></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($lmao as $key => $cate_pro)
+          <tr>
+            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]">
+              <td>{{$cate_pro->category_name}}</td>
+            <td><span class="text-ellipsis">
+              <?php
+                if($cate_pro->category_status == 0){
+                    echo "Ẩn";
+                }else{
+                  echo "Hiện";
+                }
+              ?>
+
+
+            </span></td>
+            <td>
+              <a href="" class="active" ui-toggle-class=""><i class="fa fa-pencil-square-o text-active">chọn</i><i class="fa fa-times text-danger text">   xóa</i></a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <footer class="panel-footer">
+      <div class="row">
+        
+        <div class="col-sm-5 text-center">
+          <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
+        </div>
+        <div class="col-sm-7 text-right text-center-xs">                
+          <ul class="pagination pagination-sm m-t-none m-b-none">
+            <li><a href=""><i class="fa fa-chevron-left"></i></a></li>
+            <li><a href="">1</a></li>
+            <li><a href="">2</a></li>
+            <li><a href="">3</a></li>
+            <li><a href="">4</a></li>
+            <li><a href=""><i class="fa fa-chevron-right"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+</div>
+@endsection
